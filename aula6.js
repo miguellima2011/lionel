c4945c30132b7a3561d54d6c88e0fd0888d2f66a
miguellima2011 <miguellimadasilva2011@gmail.com>
2026-04-13 19:52:45 -0300

    Iniciando projeto da atividade

---
+++ aula6.js
@@ -0,0 +1,11 @@
+const pinturafavorita = {
+    nomedapintuda: "a noite estrelada",
+    nomedoartista: "vincent van gogh",
+    ano: 1889,
+    signifcado: "uma visão da janela do hospicio de saint-remy pouco antes do amanhecer"
+};
+console.log(`${pinturafavorita.nomedapintuda }
+${pinturafavorita.nomedoartista}
+${pinturafavorita.ano}
+${pinturafavorita.signifcado}
+`)
